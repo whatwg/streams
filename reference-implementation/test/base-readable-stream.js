902c506d2d126f56fe3d60f102b25299b4971bb2
Forrest L Norvell <forrest@newrelic.com>
2014-01-21 23:52:17 -0800

    Refactor out RandomPushStream for reuse
    Too useful to not have around for other test cases. Also that test file
    was starting to look pretty long.

--- reference-implementation/test/base-readable-stream.js
+++ reference-implementation/test/base-readable-stream.js
@@ -2,80 +2,9 @@
 
 var test = require('tape');
 var Promise = require('es6-promise').Promise;
+var RandomPushStream = require('./lib/random-push-stream.js');
 
 require('../index.js');
-
-// http://stackoverflow.com/questions/1349404/generate-a-string-of-5-random-characters-in-javascript
-function randomChunk(size) {
-  var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-  for (var i = 0; i < size; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-
-  return text;
-}
-
-function RandomPushStream(toPush) {
-  this.pushed  = 0;
-  this.toPush  = toPush;
-  this.started = false;
-  this.paused  = false;
-  this.closed  = false;
-  this._handle = null;
-}
-
-RandomPushStream.prototype.readStart = function readStart() {
-  if (this.closed) return;
-
-  var stream = this;
-
-  function writeChunk() {
-    if (stream.paused) return;
-
-    stream.pushed++;
-
-    if (stream.toPush > 0 && stream.pushed > stream.toPush) {
-      if (stream._handle) {
-        clearInterval(stream._handle);
-        stream._handle = undefined;
-      }
-      stream.closed = true;
-      stream.onend();
-    }
-    else {
-      stream.ondata(randomChunk(128));
-    }
-  }
-
-  if (!this.started) {
-    this._handle = setInterval(writeChunk, 23);
-    this.started = true;
-  }
-
-  if (this.paused) {
-    this._handle = setInterval(writeChunk, 23);
-    this.paused = false;
-  }
-};
-
-RandomPushStream.prototype.readStop = function readStop() {
-  if (this.paused) return;
-
-  if (this.started) {
-    this.paused = true;
-    clearInterval(this._handle);
-    this._handle = undefined;
-  }
-  else {
-    throw new Error('can\'t pause reading an unstarted stream');
-  }
-};
-
-RandomPushStream.prototype.onend   = function onend() { };
-RandomPushStream.prototype.ondata  = function ondata() {};
-RandomPushStream.prototype.onerror = function onerror() {};
 
 function readableStreamToArray(readable) {
   return new Promise(function (resolve, reject) {
@@ -89,9 +18,7 @@ function readableStreamToArray(readable) {
         chunks.push(data);
       }
 
-      if (readable.state === 'waiting') {
-        readable.wait().then(pump);
-      }
+      if (readable.state === 'waiting') readable.wait().then(pump);
     }
 
     pump();
@@ -110,7 +37,7 @@ test('BaseReadableStream adapting a push stream', function (t) {
 
   basic = new BaseReadableStream({
     start : function start(push, close, error) {
-      t.equal(typeof push, 'function', 'push is a function in start');
+      t.equal(typeof push,  'function', 'push is a function in start');
       t.equal(typeof close, 'function', 'close is a function in start');
       t.equal(typeof error, 'function', 'error is a function in start');
 
