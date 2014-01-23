93e06136e6d5ae145a5a9178435dcedb04adc6be
Forrest L Norvell <forrest@newrelic.com>
2014-01-22 22:42:11 -0800

    Rearrange tests into logical groups

--- reference-implementation/test/base-readable-stream.js
+++ reference-implementation/test/base-readable-stream.js
@@ -25,17 +25,39 @@ function readableStreamToArray(readable) {
   });
 }
 
-test('BaseReadableStream adapting a push stream', function (t) {
+test('BaseReadableStream is globally defined', function (t) {
   /*global BaseReadableStream*/
-  var basic;
+  t.plan(1);
 
+  var basic;
   t.doesNotThrow(function () { basic = new BaseReadableStream(); },
                  'BaseReadableStream is available');
+});
 
+test('BaseReadableStream is constructed correctly', function (t) {
+  /*global BaseReadableStream*/
+  t.plan(8);
+
+  var basic = new BaseReadableStream();
+
+  t.equal(typeof basic.read, 'function', 'stream has a read function');
+  t.equal(typeof basic.wait, 'function', 'stream has a wait function');
+  t.equal(typeof basic.abort, 'function', 'stream has an abort function');
+  t.equal(typeof basic.pipeTo, 'function', 'stream has a pipeTo function');
+  t.equal(typeof basic.pipeThrough, 'function', 'stream has a pipeThrough function');
+
+  t.equal(basic.state, 'waiting', 'stream starts out waiting');
+
+  t.ok(basic.closed, 'stream has closed promise');
+  t.ok(basic.closed.then, 'stream has closed promise that is thenable');
+});
+
+test('BaseReadableStream adapting a push stream', function (t) {
+  /*global BaseReadableStream*/
   var pullChecked = false;
   var randomStream = new RandomPushStream(8);
 
-  basic = new BaseReadableStream({
+  var basic = new BaseReadableStream({
     start : function start(push, close, error) {
       t.equal(typeof push,  'function', 'push is a function in start');
       t.equal(typeof close, 'function', 'close is a function in start');
@@ -61,12 +83,6 @@ test('BaseReadableStream adapting a push stream', function (t) {
     }
   });
 
-  t.equal(basic.state, 'waiting', 'stream starts out waiting');
-  t.equal(typeof basic.read, 'function', 'stream has a read function');
-  t.equal(typeof basic.wait, 'function', 'stream has a wait function');
-  t.ok(basic.closed, 'stream has closed promise');
-  t.ok(basic.closed.then, 'stream has closed promise that is thenable');
-
   readableStreamToArray(basic).then(function (chunks) {
     t.equal(basic.state, 'closed', 'should be closed');
     t.equal(chunks.length, 8, 'got the expected 8 chunks');
@@ -76,4 +92,52 @@ test('BaseReadableStream adapting a push stream', function (t) {
 
     t.end();
   });
+});
+
+test('BaseReadableStream aborting an infinite stream', function (t) {
+  /*global BaseReadableStream, BaseWritableStream*/
+  var randomStream = new RandomPushStream();
+
+  var readable = new BaseReadableStream({
+    start : function start(push, close, error) {
+      randomStream.ondata  = push;
+      randomStream.onend   = close;
+      randomStream.onerror = error;
+    },
+
+    pull : function pull() { randomStream.readStart(); },
+
+    abort : function abort() {
+      randomStream.readStop();
+      randomStream.onend();
+    }
+  });
+
+  var storage = [];
+  var writable = new BaseWritableStream({
+    write : function write(data, done) {
+      storage.push(data);
+      done();
+    }
+  });
+
+  readable.pipeTo(writable);
+
+  readable.closed.then(function () {
+    t.equal(readable.state, 'closed', 'readable should be closed');
+  });
+
+  writable.closed.then(function () {
+    t.equal(writable.state, 'closed', 'writable should be closed');
+    t.ok(storage.length > 0, 'should have gotten some data written through the pipe');
+    for (var i = 0; i < storage.length; i++) {
+      t.equal(storage[i].length, 128, 'each chunk has 128 bytes');
+    }
+
+    t.end();
+  });
+
+  setTimeout(function () {
+    readable.abort(new Error('don\'t feel like dealing with randomness anymore'));
+  }, 150);
 });
--- reference-implementation/test/base-writable-stream.js
+++ reference-implementation/test/base-writable-stream.js
@@ -11,15 +11,36 @@ function writeArrayToStream(array, writableStream) {
   return writableStream.close();
 }
 
-test('BaseWritableStream tests', function (t) {
+test('BaseWritableStream is globally defined', function (t) {
   /*global BaseWritableStream*/
+  t.plan(1);
+
   var basic;
   t.doesNotThrow(function () { basic = new BaseWritableStream(); },
                  'BaseWritableStream is available');
+});
 
+test('BaseWritableStream is correctly constructed', function (t) {
+  /*global BaseWritableStream*/
+  t.plan(7);
+
+  var basic = new BaseWritableStream();
+
+  t.equal(typeof basic.write, 'function', 'has write function');
+  t.equal(typeof basic.wait, 'function', 'has wait function');
+  t.equal(typeof basic.abort, 'function', 'has abort function');
+  t.equal(typeof basic.close, 'function', 'has close function');
+
+  t.equal(basic.state, 'waiting', 'stream has default new state');
+
+  t.ok(basic.closed, 'has closed promise');
+  t.ok(basic.closed.then, 'has closed promise that is thenable');
+});
+
+test('BaseWritableStream with simple input', function (t) {
+  /*global BaseWritableStream*/
   var storage;
-
-  basic = new BaseWritableStream({
+  var basic = new BaseWritableStream({
     start : function start() { storage = []; },
 
     write : function write(data, done) {
@@ -38,17 +59,9 @@ test('BaseWritableStream tests', function (t) {
     }
   });
 
-  t.equal(basic.state, 'waiting', 'stream has default new state');
-  t.ok(basic.closed, 'has closed accessor');
-  t.ok(basic.write,  'has write function');
-  t.ok(basic.wait,   'has write function');
-  t.ok(basic.close,  'has close function');
-  t.ok(basic.abort,  'has abort function');
-
   var input = [1, 2, 3, 4, 5];
   writeArrayToStream(input, basic).then(function () {
     t.deepEqual(storage, input, 'got back what was passed in');
-
     t.end();
   }, function (error) {
     t.fail(error);
