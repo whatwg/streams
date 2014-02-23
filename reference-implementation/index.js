7740950437faf574cab4a6242d6271e792466ff0
Domenic Denicola <domenic@domenicdenicola.com>
2014-02-22 22:58:36 -0500

    Remove stubs that are unlikely to make it.
    At least for the initial API, it is unlikely that ReadableStreamWatcher and WritableStreamWatcher will make it. CorkableWritableStream I am more optimistic about, but we should remove it for now until we get more buy-in and flesh it out.

--- reference-implementation/index.js
+++ reference-implementation/index.js
@@ -2,13 +2,10 @@
 
 global.BaseReadableStream = require('./lib/base-readable.js');
 global.ReadableStream = function () {};
+
 global.BaseWritableStream = require('./lib/base-writable.js');
 global.WritableStream = function () {};
-global.CorkableWritableStream = function () {};
-global.TeeStream = function () {};
 
+global.TeeStream = function () {};
 global.LengthBufferingStrategy = function () {};
 global.CountBufferingStrategy = function () {};
-
-global.ReadableStreamWatcher = function () {};
-global.WritableStreamWatcher = function () {};
