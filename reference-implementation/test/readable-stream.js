ff6c8bf822da4017440e501044d301f08db3cb3a
Domenic Denicola <domenic@domenicdenicola.com>
2014-06-30 14:11:15 -0400

    Move cancellation tests to their own file.

--- reference-implementation/test/readable-stream.js
+++ reference-implementation/test/readable-stream.js
@@ -218,55 +218,6 @@ test('ReadableStream adapting an async pull source', t => {
   });
 });
 
-test('ReadableStream canceling an infinite stream', t => {
-  var randomSource = new RandomPushSource();
-
-  var cancelationFinished = false;
-  var rs = new ReadableStream({
-    start(enqueue, close, error) {
-      randomSource.ondata = enqueue;
-      randomSource.onend = close;
-      randomSource.onerror = error;
-    },
-
-    pull() {
-      randomSource.readStart();
-    },
-
-    cancel() {
-      randomSource.readStop();
-      randomSource.onend();
-
-      return new Promise(resolve => setTimeout(() => {
-        cancelationFinished = true;
-        resolve();
-      }, 50));
-    }
-  });
-
-  readableStreamToArray(rs).then(
-    storage => {
-      t.equal(rs.state, 'closed', 'stream should be closed');
-      t.equal(cancelationFinished, false, 'it did not wait for the cancellation process to finish before closing');
-      t.ok(storage.length > 0, 'should have gotten some data written through the pipe');
-      for (var i = 0; i < storage.length; i++) {
-        t.equal(storage[i].length, 128, 'each chunk has 128 bytes');
-      }
-    },
-    () => {
-      t.fail('the stream should be successfully read to the end');
-      t.end();
-    }
-  );
-
-  setTimeout(() => {
-    rs.cancel().then(() => {
-      t.equal(cancelationFinished, true, 'it returns a promise that waits for the cancellation to finish');
-      t.end();
-    });
-  }, 150);
-});
-
 test('ReadableStream is able to pull data repeatedly if it\'s available synchronously', t => {
   var i = 0;
   var rs = new ReadableStream({
@@ -350,69 +301,6 @@ test('ReadableStream should be able to get data sequentially from an asynchronou
       }
     });
   }
-});
-
-test('ReadableStream cancellation puts the stream in a closed state (no chunks pulled yet)', t => {
-  var rs = sequentialReadableStream(5);
-
-  t.plan(5);
-
-  rs.closed.then(
-    () => t.assert(true, 'closed promise vended before the cancellation should fulfill'),
-    () => t.fail('closed promise vended before the cancellation should not be rejected')
-  );
-
-  rs.wait().then(
-    () => t.assert(true, 'wait() promise vended before the cancellation should fulfill'),
-    () => t.fail('wait() promise vended before the cancellation should not be rejected')
-  );
-
-  rs.cancel();
-
-  t.equal(rs.state, 'closed', 'state should be closed');
-
-  rs.closed.then(
-    () => t.assert(true, 'closed promise vended after the cancellation should fulfill'),
-    () => t.fail('closed promise vended after the cancellation should not be rejected')
-  );
-  rs.wait().then(
-    () => t.assert(true, 'wait promise vended after the cancellation should fulfill'),
-    () => t.fail('wait promise vended after the cancellation should not be rejected')
-  );
-});
-
-test('ReadableStream cancellation puts the stream in a closed state (after waiting for chunks)', t => {
-  var rs = sequentialReadableStream(5);
-
-  t.plan(5);
-
-  rs.wait().then(
-    () => {
-      rs.closed.then(
-        () => t.assert(true, 'closed promise vended before the cancellation should fulfill'),
-        () => t.fail('closed promise vended before the cancellation should not be rejected')
-      );
-
-      rs.wait().then(
-        () => t.assert(true, 'wait() promise vended before the cancellation should fulfill'),
-        () => t.fail('wait() promise vended before the cancellation should not be rejected')
-      );
-
-      rs.cancel();
-
-      t.equal(rs.state, 'closed', 'state should be closed');
-
-      rs.closed.then(
-        () => t.assert(true, 'closed promise vended after the cancellation should fulfill'),
-        () => t.fail('closed promise vended after the cancellation should not be rejected')
-      );
-      rs.wait().then(
-        () => t.assert(true, 'wait promise vended after the cancellation should fulfill'),
-        () => t.fail('wait promise vended after the cancellation should not be rejected')
-      );
-    },
-    r => t.ifError(r)
-  );
 });
 
 test('ReadableStream returns `true` for the first `enqueue` call; `false` thereafter, if nobody reads', t => {
