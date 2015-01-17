7676d810d481efdb9339f4eaf27c777e97e17349
Domenic Denicola <d@domenic.me>
2015-01-16 19:10:11 -0500

    Add tests for the new source/sink error cases
    The previous commits introduced many new places that we call in to user code, since we no longer validate that we are grabbing functions and then store them, but instead extract and invoke methods every time. This commit adds tests for a variety of pathological inputs, to ensure that the errors they throw are propagated or caught-and-exposed, as appropriate.

---
+++ reference-implementation/test/bad-underlying-sinks.js
@@ -0,0 +1,314 @@
+var test = require('tape');
+
+import WritableStream from '../lib/writable-stream';
+
+test('Throwing underlying sink start getter', t => {
+  var theError = new Error('a unique string');
+
+  t.throws(() => {
+    new WritableStream({
+      get start() {
+        throw theError;
+      }
+    });
+  }, /a unique string/);
+  t.end();
+});
+
+test('Throwing underlying sink start method', t => {
+  var theError = new Error('a unique string');
+
+  t.throws(() => {
+    new WritableStream({
+      start() {
+        throw theError;
+      }
+    });
+  }, /a unique string/);
+  t.end();
+});
+
+test('Throwing underlying source write getter', t => {
+  t.plan(2);
+
+  var theError = new Error('a unique string');
+  var ws = new WritableStream({
+    get write() {
+      throw theError;
+    }
+  });
+
+  ws.write('a').then(
+    () => t.fail('write should not fulfill'),
+    r => t.equal(r, theError, 'write should reject with the thrown error')
+  );
+
+  ws.closed.then(
+    () => t.fail('closed should not fulfill'),
+    r => t.equal(r, theError, 'closed should reject with the thrown error')
+  );
+});
+
+test('Throwing underlying source write method', t => {
+  t.plan(2);
+
+  var theError = new Error('a unique string');
+  var ws = new WritableStream({
+    write() {
+      throw theError;
+    }
+  });
+
+  ws.write('a').then(
+    () => t.fail('write should not fulfill'),
+    r => t.equal(r, theError, 'write should reject with the thrown error')
+  );
+
+  ws.closed.then(
+    () => t.fail('closed should not fulfill'),
+    r => t.equal(r, theError, 'closed should reject with the thrown error')
+  );
+});
+
+test('Throwing underlying sink abort getter', t => {
+  t.plan(2);
+
+  var theError = new Error('a unique string');
+  var abortReason = new Error('different string');
+  var ws = new WritableStream({
+    get abort() {
+      throw theError;
+    }
+  });
+
+  ws.abort(abortReason).then(
+    () => t.fail('abort should not fulfill'),
+    r => t.equal(r, theError, 'abort should reject with the abort reason')
+  );
+
+  ws.closed.then(
+    () => t.fail('closed should not fulfill'),
+    r => t.equal(r, abortReason, 'closed should reject with the thrown error')
+  );
+});
+
+test('Throwing underlying sink abort method', t => {
+  t.plan(2);
+
+  var theError = new Error('a unique string');
+  var abortReason = new Error('different string');
+  var ws = new WritableStream({
+    abort() {
+      throw theError;
+    }
+  });
+
+  ws.abort(abortReason).then(
+    () => t.fail('abort should not fulfill'),
+    r => t.equal(r, theError, 'abort should reject with the abort reason')
+  );
+
+  ws.closed.then(
+    () => t.fail('closed should not fulfill'),
+    r => t.equal(r, abortReason, 'closed should reject with the thrown error')
+  );
+});
+
+test('Throwing underlying sink close getter', t => {
+  t.plan(1);
+
+  var theError = new Error('a unique string');
+  var ws = new WritableStream({
+    get close() {
+      throw theError;
+    }
+  });
+
+  ws.close().then(
+    () => t.fail('close should not fulfill'),
+    r => t.equal(r, theError, 'close should reject with the thrown error')
+  );
+});
+
+test('Throwing underlying sink close method', t => {
+  t.plan(1);
+
+  var theError = new Error('a unique string');
+  var ws = new WritableStream({
+    close() {
+      throw theError;
+    }
+  });
+
+  ws.close().then(
+    () => t.fail('close should not fulfill'),
+    r => t.equal(r, theError, 'close should reject with the thrown error')
+  );
+});
+
+test('Throwing underlying source strategy getter: initial construction', t => {
+  var theError = new Error('a unique string');
+
+  t.throws(() => {
+    new WritableStream({
+      get strategy() {
+        throw theError;
+      }
+    });
+  }, /a unique string/);
+  t.end();
+});
+
+test('Throwing underlying source strategy getter: first write', t => {
+  t.plan(2);
+
+  var counter = 0;
+  var theError = new Error('a unique string');
+  var ws = new WritableStream({
+    get strategy() {
+      ++counter;
+      if (counter === 1) {
+        return {
+          size() {
+            return 1;
+          },
+          shouldApplyBackpressure() {
+            return true;
+          }
+        };
+      }
+
+      throw theError;
+    }
+  });
+
+  ws.write('a').then(
+    () => t.fail('write should not fulfill'),
+    r => t.equal(r, theError, 'write should reject with the thrown error')
+  );
+
+  ws.closed.then(
+    () => t.fail('closed should not fulfill'),
+    r => t.equal(r, theError, 'closed should reject with the thrown error')
+  );
+});
+
+test('Throwing underlying source strategy.size getter: initial construction', t => {
+  t.doesNotThrow(() => {
+    new WritableStream({
+      strategy: {
+        get size() {
+          throw new Error('boo');
+        },
+        shouldApplyBackpressure() {
+          return true;
+        }
+      }
+    });
+  });
+  t.end();
+});
+
+test('Throwing underlying source strategy.size method: initial construction', t => {
+  t.doesNotThrow(() => {
+    new WritableStream({
+      strategy: {
+        size() {
+          throw new Error('boo');
+        },
+        shouldApplyBackpressure() {
+          return true;
+        }
+      }
+    });
+  });
+  t.end();
+});
+
+test('Throwing underlying source strategy.size getter: first write', t => {
+  t.plan(2);
+
+  var theError = new Error('a unique string');
+  var ws = new WritableStream({
+    strategy: {
+      get size() {
+        throw theError;
+      },
+      shouldApplyBackpressure() {
+        return true;
+      }
+    }
+  });
+
+  ws.write('a').then(
+    () => t.fail('write should not fulfill'),
+    r => t.equal(r, theError, 'write should reject with the thrown error')
+  );
+
+  ws.closed.then(
+    () => t.fail('closed should not fulfill'),
+    r => t.equal(r, theError, 'closed should reject with the thrown error')
+  );
+});
+
+test('Throwing underlying source strategy.size method: first write', t => {
+  t.plan(2);
+
+  var theError = new Error('a unique string');
+  var ws = new WritableStream({
+    strategy: {
+      size() {
+        throw theError;
+      },
+      shouldApplyBackpressure() {
+        return true;
+      }
+    }
+  });
+
+  ws.write('a').then(
+    () => t.fail('write should not fulfill'),
+    r => t.equal(r, theError, 'write should reject with the thrown error')
+  );
+
+  ws.closed.then(
+    () => t.fail('closed should not fulfill'),
+    r => t.equal(r, theError, 'closed should reject with the thrown error')
+  );
+});
+
+test('Throwing underlying source strategy.shouldApplyBackpressure getter: initial construction', t => {
+  var theError = new Error('a unique string');
+
+  t.throws(() => {
+    new WritableStream({
+      strategy: {
+        size() {
+          return 1;
+        },
+        get shouldApplyBackpressure() {
+          throw theError;
+        }
+      }
+    });
+  }, /a unique string/);
+  t.end();
+});
+
+test('Throwing underlying source strategy.shouldApplyBackpressure method: initial construction', t => {
+  var theError = new Error('a unique string');
+
+  t.throws(() => {
+    new WritableStream({
+      strategy: {
+        size() {
+          return 1;
+        },
+        shouldApplyBackpressure() {
+          throw theError;
+        }
+      }
+    });
+  }, /a unique string/);
+  t.end();
+});
---
+++ reference-implementation/test/bad-underlying-sources.js
@@ -0,0 +1,239 @@
+var test = require('tape');
+
+import ReadableStream from '../lib/readable-stream';
+
+test('Throwing underlying source start getter', t => {
+  var theError = new Error('a unique string');
+
+  t.throws(() => {
+    new ReadableStream({
+      get start() {
+        throw theError;
+      }
+    });
+  }, /a unique string/);
+  t.end();
+});
+
+test('Throwing underlying source start method', t => {
+  var theError = new Error('a unique string');
+
+  t.throws(() => {
+    new ReadableStream({
+      start() {
+        throw theError;
+      }
+    });
+  }, /a unique string/);
+  t.end();
+});
+
+test('Throwing underlying source pull getter (initial pull)', t => {
+  t.plan(1);
+
+  var theError = new Error('a unique string');
+  var rs = new ReadableStream({
+    get pull() {
+      throw theError;
+    }
+  });
+
+  rs.closed.then(
+    () => t.fail('closed should not fulfill'),
+    r => t.equal(r, theError, 'closed should reject with the thrown error')
+  );
+});
+
+test('Throwing underlying source pull method (initial pull)', t => {
+  t.plan(1);
+
+  var theError = new Error('a unique string');
+  var rs = new ReadableStream({
+    pull() {
+      throw theError;
+    }
+  });
+
+  rs.closed.then(
+    () => t.fail('closed should not fulfill'),
+    r => t.equal(r, theError, 'closed should reject with the thrown error')
+  );
+});
+
+test('Throwing underlying source pull getter (second pull)', t => {
+  t.plan(3);
+
+  var theError = new Error('a unique string');
+  var counter = 0;
+  var rs = new ReadableStream({
+    get pull() {
+      ++counter;
+      if (counter === 1) {
+        return enqueue => enqueue('a');
+      }
+
+      throw theError;
+    }
+  });
+
+  rs.ready.then(() => {
+    t.equal(rs.state, 'readable', 'sanity check: the stream becomes readable without issue');
+
+    t.throws(() => rs.read(), /a unique string/, 'reading triggers a pull, and the error is re-thrown');
+  });
+
+  rs.closed.then(
+    () => t.fail('closed should not fulfill'),
+    r => t.equal(r, theError, 'closed should reject with the thrown error')
+  );
+});
+
+test('Throwing underlying source pull method (second pull)', t => {
+  t.plan(3);
+
+  var theError = new Error('a unique string');
+  var counter = 0;
+  var rs = new ReadableStream({
+    pull(enqueue) {
+      ++counter;
+      if (counter === 1) {
+        enqueue('a');
+      } else {
+        throw theError;
+      }
+    }
+  });
+
+  rs.ready.then(() => {
+    t.equal(rs.state, 'readable', 'sanity check: the stream becomes readable without issue');
+
+    t.throws(() => rs.read(), /a unique string/, 'reading triggers a pull, and the error is re-thrown');
+  });
+
+  rs.closed.then(
+    () => t.fail('closed should not fulfill'),
+    r => t.equal(r, theError, 'closed should reject with the thrown error')
+  );
+});
+
+test('Throwing underlying source cancel getter', t => {
+  t.plan(1);
+
+  var theError = new Error('a unique string');
+  var rs = new ReadableStream({
+    get cancel() {
+      throw theError;
+    }
+  });
+
+  rs.cancel().then(
+    () => t.fail('cancel should not fulfill'),
+    r => t.equal(r, theError, 'cancel should reject with the thrown error')
+  );
+});
+
+test('Throwing underlying source cancel method', t => {
+  t.plan(1);
+
+  var theError = new Error('a unique string');
+  var rs = new ReadableStream({
+    cancel() {
+      throw theError;
+    }
+  });
+
+  rs.cancel().then(
+    () => t.fail('cancel should not fulfill'),
+    r => t.equal(r, theError, 'cancel should reject with the thrown error')
+  );
+});
+
+test('Throwing underlying source strategy getter', t => {
+  var theError = new Error('a unique string');
+
+  new ReadableStream({
+    start(enqueue) {
+      t.throws(() => enqueue('a'), /a unique string/);
+      t.end();
+    },
+    get strategy() {
+      throw theError;
+    }
+  });
+});
+
+test('Throwing underlying source strategy.size getter', t => {
+  var theError = new Error('a unique string');
+
+  new ReadableStream({
+    start(enqueue) {
+      t.throws(() => enqueue('a'), /a unique string/);
+      t.end();
+    },
+    strategy: {
+      get size() {
+        throw theError;
+      },
+      shouldApplyBackpressure() {
+        return true;
+      }
+    }
+  });
+});
+
+test('Throwing underlying source strategy.size method', t => {
+  var theError = new Error('a unique string');
+
+  new ReadableStream({
+    start(enqueue) {
+      t.throws(() => enqueue('a'), /a unique string/);
+      t.end();
+    },
+    strategy: {
+      size() {
+        throw theError;
+      },
+      shouldApplyBackpressure() {
+        return true;
+      }
+    }
+  });
+});
+
+test('Throwing underlying source strategy.shouldApplyBackpressure getter', t => {
+  var theError = new Error('a unique string');
+
+  new ReadableStream({
+    start(enqueue) {
+      t.throws(() => enqueue('a'), /a unique string/);
+      t.end();
+    },
+    strategy: {
+      size() {
+        return 1;
+      },
+      get shouldApplyBackpressure() {
+        throw theError;
+      }
+    }
+  });
+});
+
+test('Throwing underlying source strategy.shouldApplyBackpressure method', t => {
+  var theError = new Error('a unique string');
+
+  new ReadableStream({
+    start(enqueue) {
+      t.throws(() => enqueue('a'), /a unique string/);
+      t.end();
+    },
+    strategy: {
+      size() {
+        return 1;
+      },
+      shouldApplyBackpressure() {
+        throw theError;
+      }
+    }
+  });
+});
