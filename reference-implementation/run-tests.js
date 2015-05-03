b52806e55891d6698e25ef4fcf1c4225d4797fe4
Domenic Denicola <d@domenic.me>
2015-05-03 21:42:18 +0200

    Fix tests
    Broken since 5b47faa994139c3c68bb462920f58a29372f6784.... :-/

--- reference-implementation/run-tests.js
+++ reference-implementation/run-tests.js
@@ -3,14 +3,12 @@ const path = require('path');
 
 import ReadableStream from './lib/readable-stream';
 import WritableStream from './lib/writable-stream';
-import ReadableByteStream from './lib/experimental/readable-byte-stream';
 import ByteLengthQueuingStrategy from './lib/byte-length-queuing-strategy';
 import CountQueuingStrategy from './lib/count-queuing-strategy';
 import TransformStream from './lib/transform-stream';
 
 global.ReadableStream = ReadableStream;
 global.WritableStream = WritableStream;
-global.ReadableByteStream = ReadableByteStream;
 global.ByteLengthQueuingStrategy = ByteLengthQueuingStrategy;
 global.CountQueuingStrategy = CountQueuingStrategy;
 global.TransformStream = TransformStream;
@@ -19,9 +17,7 @@ global.TransformStream = TransformStream;
 if (process.argv.length === 2) {
   const tests = glob.sync(path.resolve(__dirname, 'test/*.js'));
 
-  // disable experimental tests while we figure out impact of async read on ReadableByteStream
-  const experimentalTests = []; // glob.sync(path.resolve(__dirname, 'test/experimental/*.js'));
-  tests.concat(experimentalTests).forEach(require);
+  tests.forEach(require);
 } else {
     glob.sync(path.resolve(process.argv[2])).forEach(require);
 }
