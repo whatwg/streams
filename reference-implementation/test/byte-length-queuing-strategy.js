1458cf3b87112577f51a747153c9620e5d38af8d
Xabier Rodriguez Calvar <calvaris@igalia.com>
2015-07-23 11:32:28 +0200

    Added constructor parameter test for byte length queuing strategy

--- reference-implementation/test/byte-length-queuing-strategy.js
+++ reference-implementation/test/byte-length-queuing-strategy.js
@@ -15,6 +15,25 @@ test('Can construct a ByteLengthQueuingStrategy with any value as its high water
   t.end();
 });
 
+test('ByteLengthQueuingStrategy constructor behaves as expected with wrong arguments', t => {
+  const highWaterMark = 1;
+  const highWaterMarkObjectGetter = {
+    get highWaterMark() { return highWaterMark; },
+  };
+  const error = new Error("wow!");
+  const highWaterMarkObjectGetterThrowing = {
+    get highWaterMark() { throw error; },
+  };
+  t.throws(() => new ByteLengthQueuingStrategy(), /TypeError/, 'construction fails with undefined');
+  t.throws(() => new ByteLengthQueuingStrategy(null), /TypeError/, 'construction fails with null');
+  t.doesNotThrow(() => new ByteLengthQueuingStrategy('potato'), 'construction succeeds with a random non-object type');
+  t.doesNotThrow(() => new ByteLengthQueuingStrategy({}), 'construction succeeds with an object without hwm property');
+  t.doesNotThrow(() => new ByteLengthQueuingStrategy(highWaterMarkObjectGetter), 'construction succeeds with an object with a hwm getter');
+  t.throws(() => new ByteLengthQueuingStrategy(highWaterMarkObjectGetterThrowing), /wow/, 'construction fails with the error thrown by the getter');
+
+  t.end();
+});
+
 test('ByteLengthQueuingStrategy instances have the correct properties', t => {
   const strategy = new ByteLengthQueuingStrategy({ highWaterMark: 4 });
 
