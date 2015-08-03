427b7628bd4414016a5f0d6e1fadfffd7eed984c
Domenic Denicola <d@domenic.me>
2015-08-03 16:31:05 -0400

    Notate throwing vs. non-throwing abstract operations
    Fixes #371.

--- ecmarkupify.js
+++ ecmarkupify.js
@@ -9,7 +9,7 @@ export default ecmarkupify;
 
 // Can't use usual trick of module.parent === null because we intend to be run via traceur-runner
 if (module.parent == require.main) {
-  ecmarkupify(process.argv[2], process.argv[3]);
+  ecmarkupify(process.argv[2], process.argv[3]).catch(e => setTimeout(() => { throw e; }, 0));
 }
 
 
@@ -25,6 +25,7 @@ function ecmarkupify(inputFile, outputFile) {
   ])
   .then(() => {
     addAllAOIDsToBiblio(spec);
+    addThrowingTags(spec.doc);
     spec.buildAlgs();
 
     fs.writeFileSync(outputFile, jsdom.serializeDocument(spec.doc));
@@ -50,5 +51,27 @@ function addAllAOIDsToBiblio(spec) {
   for (const ao of aos) {
     const aoid = ao.getAttribute('aoid');
     spec.biblio.ops[aoid] = '#' + ao.id;
+  }
+}
+
+function addThrowingTags(doc) {
+  const aos = Array.from(doc.querySelectorAll('[aoid]'));
+  for (const ao of aos) {
+    const hasThrows = ao.hasAttribute('throws');
+    const hasNothrow = ao.hasAttribute('nothrow');
+
+    if ((!hasThrows && !hasNothrow) || (hasThrows && hasNothrow)) {
+      throw new Error('All abstract operations must be notated with exactly one of the throws or nothrow ' +
+        `attributes. Check the abstract operation with aoid="${ao.getAttribute("aoid")}"`);
+    }
+
+    const labelEl = doc.createElement('span');
+    labelEl.className = 'annotation';
+    labelEl.textContent = hasThrows ? 'throws' : 'nothrow';
+    labelEl.title = hasThrows ? 'can return an abrupt completion' : 'never returns an abrupt completion';
+
+    const insertBeforeThis = ao.querySelector('.self-link');
+    ao.insertBefore(labelEl, insertBeforeThis);
+    ao.insertBefore(doc.createTextNode(' '), labelEl);
   }
 }
