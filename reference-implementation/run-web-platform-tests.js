eb9bb0cbada0165c3fdad61825856e480f9f60f8
Takeshi Yoshino <tyoshino@chromium.org>
2016-08-25 15:11:52 +0900

    Convert some general tests for ws into wpt
    This commit is for the issue #492.
    
    wpt-runner invocation is tweaked by domenic.
    
    We'll use the to-upstream-wpts/ directory for placing converted wpt files until it's ready for upstreaming them to the w3c repo.
    
    All the tests under the directory are run automatically when we run "npm run wpt" in addition to the ones already moved to the w3c repo.

--- reference-implementation/run-web-platform-tests.js
+++ reference-implementation/run-web-platform-tests.js
@@ -10,8 +10,10 @@ const ByteLengthQueuingStrategy = require('./lib/byte-length-queuing-strategy.js
 const CountQueuingStrategy = require('./lib/count-queuing-strategy.js');
 
 const testsPath = path.resolve(__dirname, 'web-platform-tests/streams');
+const toUpstreamTestsPath = path.resolve(__dirname, 'to-upstream-wpts');
 
-wptRunner(testsPath, setup)
+wptRunner(toUpstreamTestsPath, { rootURL: 'streams/', setup })
+  .then(() => wptRunner(testsPath, { rootURL: 'streams/', setup }))
   .then(failures => process.exit(failures))
   .catch(e => {
     console.error(e.stack);
@@ -23,6 +25,7 @@ function setup(window) {
   window.Promise = Promise;
 
   window.ReadableStream = ReadableStream;
+  window.WritableStream = WritableStream;
   window.ByteLengthQueuingStrategy = ByteLengthQueuingStrategy;
   window.CountQueuingStrategy = CountQueuingStrategy;
 }
