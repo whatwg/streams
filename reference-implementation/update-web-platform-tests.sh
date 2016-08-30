d04ef6e811ecdded7b75956dc263f6ff2ff2f628
Domenic Denicola <d@domenic.me>
2016-08-30 15:11:11 -0400

    Continue trying to fix the build
    See http://stackoverflow.com/q/11656761/3191

--- reference-implementation/update-web-platform-tests.sh
+++ reference-implementation/update-web-platform-tests.sh
@@ -3,7 +3,8 @@ set -e
 
 if [ -d "web-platform-tests" ]; then
   cd web-platform-tests
-  git pull origin master
+  git fetch
+  git reset --hard origin/master
 else
   git clone --depth 1 https://github.com/w3c/web-platform-tests.git
 fi
