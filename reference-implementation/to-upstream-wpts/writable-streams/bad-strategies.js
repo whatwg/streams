bf0e8965f38c228d464dc97aa3d71dddc36da300
isonmad <isonmad@github>
2016-11-03 13:22:26 +0000

    address readability

--- reference-implementation/to-upstream-wpts/writable-streams/bad-strategies.js
+++ reference-implementation/to-upstream-wpts/writable-streams/bad-strategies.js
@@ -4,7 +4,10 @@ if (self.importScripts) {
   self.importScripts('/resources/testharness.js');
 }
 
-test(() => assert_throws(new TypeError(), () => new WritableStream({}, { size: 'a string' })),
-     'reject any non-function value for strategy.size');
+test(() => {
+  assert_throws(new TypeError(), () => {
+    new WritableStream({}, { size: 'a string' });
+  });
+}, 'reject any non-function value for strategy.size');
 
 done();
