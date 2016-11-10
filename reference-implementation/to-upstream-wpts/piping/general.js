82b11ea675bda40526c2b490dc74e276fccd5424
isonmad <isonmad@github>
2016-11-10 04:32:48 +0000

    split out transform-streams.js

--- reference-implementation/to-upstream-wpts/piping/general.js
+++ reference-implementation/to-upstream-wpts/piping/general.js
@@ -154,24 +154,4 @@ promise_test(() => {
 
 }, 'Piping from a ReadableStream for which a chunk becomes asynchronously readable after the pipeTo');
 
-promise_test(() => {
-  const rs = new ReadableStream({
-    start(c) {
-      c.enqueue('a');
-      c.enqueue('b');
-      c.enqueue('c');
-      c.close();
-    }
-  });
-
-  const ts = new TransformStream();
-
-  const ws = new WritableStream();
-
-  return rs.pipeThrough(ts).pipeTo(ws).then(() => {
-    const writer = ws.getWriter();
-    return writer.closed;
-  });
-}, 'Piping through an identity transform stream should close the destination when the source closes');
-
 done();
