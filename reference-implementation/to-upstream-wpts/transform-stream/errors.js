ae749d629a0ac8308883653e35fd476520a2b88e
isonmad <github2.nobled@spamgourmet.com>
2016-11-09 23:34:10 -0500

    WPT-ify most of the remaining tape tests (#604)
    * port bad-strategies.js to WPT
    * port brand-checks.js to WPT
    * port byte-length-queuing-strategy.js to WPT
    * port count-queuing-strategy.js to WPT
    * port transform-stream-errors.js to WPT
    * put sync tests first, since they show up in the results as completed first.
    * put size tests together

---
+++ reference-implementation/to-upstream-wpts/transform-stream/errors.js
@@ -0,0 +1,57 @@
+'use strict';
+
+if (self.importScripts) {
+  self.importScripts('/resources/testharness.js');
+}
+
+const thrownError = new Error('bad things are happening!');
+thrownError.name = 'error1';
+
+promise_test(t => {
+  const ts = new TransformStream({
+    transform() {
+      throw thrownError;
+    }
+  });
+
+  const reader = ts.readable.getReader();
+
+  const writer = ts.writable.getWriter();
+
+  writer.write('a');
+
+  return Promise.all([
+    promise_rejects(t, thrownError, reader.read(),
+                    'readable\'s read should reject with the thrown error'),
+    promise_rejects(t, thrownError, reader.closed,
+                    'readable\'s closed should be rejected with the thrown error'),
+    promise_rejects(t, thrownError, writer.closed,
+                    'writable\'s closed should be rejected with the thrown error')
+  ]);
+}, 'TransformStream errors thrown in transform put the writable and readable in an errored state');
+
+promise_test(t => {
+  const ts = new TransformStream({
+    transform() {
+    },
+    flush() {
+      throw thrownError;
+    }
+  });
+
+  const reader = ts.readable.getReader();
+
+  const writer = ts.writable.getWriter();
+
+  writer.write('a');
+  writer.close();
+
+  return Promise.all([
+    promise_rejects(t, thrownError, reader.read(),
+                    'readable\'s read should reject with the thrown error'),
+    promise_rejects(t, thrownError, reader.closed,
+                    'readable\'s closed should be rejected with the thrown error'),
+    promise_rejects(t, thrownError, writer.closed,
+                    'writable\'s closed should be rejected with the thrown error')
+  ]);
+}, 'TransformStream errors thrown in flush put the writable and readable in an errored state');
