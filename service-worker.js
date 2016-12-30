2ad94882f75005110591268ac4dad34925bdeafb
Domenic Denicola <d@domenic.me>
2016-12-30 13:19:24 -0500

    Do cache-then-network for everything but the main resource
    This is much snappier!!

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,6 @@
 "use strict";
-// Largely based on https://css-tricks.com/serviceworker-for-offline/
 
-const cacheKey = "v1";
+const cacheKey = "v2";
 const toCache = [
   "/",
   "https://resources.whatwg.org/standard.css",
@@ -20,24 +19,33 @@ self.onfetch = e => {
     return;
   }
 
-  e.respondWith(
+  if (needsToBeFresh(e.request)) {
     // Since this is a Living Standard, it is imperative that you see the freshest content, so we use a
-    // network-then-cache strategy.
-    fetch(e.request).then(res => {
-      if (!res.ok) {
-        throw new Error(`${res.url} is responding with ${res.status}; falling back to cache if possible`);
-      }
+    // network-then-cache strategy for the main content.
+    e.respondWith(
+      fetch(e.request).then(res => {
+        refreshCacheFromNetworkResponse(e.request, res);
+        return res;
+      })
+      .catch(() => {
+        return caches.match(e.request);
+      })
+    );
+  } else {
+    // For auxiliary resources, we can use a cache-then-network strategy; it is OK to not get the freshest.
+    e.respondWith(
+      caches.match(e.request).then(cachedResponse => {
+        const networkFetchPromise = fetch(e.request);
 
-      const responseForCache = res.clone();
-      // Do not return this promise; it's OK if caching fails, and we don't want to block on it.
-      caches.open(cacheKey).then(cache => cache.put(e.request, responseForCache));
+        // Ignore network fetch or caching errors; they just mean we won't be able to refresh the cache.
+        networkFetchPromise
+          .then(res => refreshCacheFromNetworkResponse(e.request, res))
+          .catch(() => {});
 
-      return res;
-    })
-    .catch(() => {
-      return caches.match(e.request);
-    })
-  );
+        return cachedResponse || networkFetchPromise;
+      })
+    );
+  }
 };
 
 self.onactivate = e => {
@@ -45,3 +53,19 @@ self.onactivate = e => {
     return Promise.all(keys.filter(key => key !== cacheKey).map(key => caches.delete(key)));
   }));
 };
+
+function refreshCacheFromNetworkResponse(req, res) {
+  if (!res.ok) {
+    throw new Error(`${res.url} is responding with ${res.status}`);
+  }
+
+  const resForCache = res.clone();
+
+  // Do not return this promise; it's OK if caching fails, and we don't want to block on it.
+  caches.open(cacheKey).then(cache => cache.put(req, resForCache));
+}
+
+function needsToBeFresh(req) {
+  const requestURL = new URL(req.url);
+  return requestURL.origin === location.origin && requestURL.pathname === "/";
+}
