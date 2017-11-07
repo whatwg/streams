12a8588fae4acb9eaf6336f530ced30e820f7141
Adam Rice <ricea@chromium.org>
2017-11-07 20:37:51 +0900

    Remove transform streams tests as they are now upstream
    Also remove the to-upstream-wpts directory and all mentions of it.
    
    Also update the web-platform-tests pointer to include the upstreamed
    tests.
    
    Closes #851.

--- reference-implementation/run-web-platform-tests.js
+++ reference-implementation/run-web-platform-tests.js
@@ -13,7 +13,6 @@ const ByteLengthQueuingStrategy = require('./lib/byte-length-queuing-strategy.js
 const CountQueuingStrategy = require('./lib/count-queuing-strategy.js');
 
 const testsPath = path.resolve(__dirname, 'web-platform-tests/streams');
-const toUpstreamTestsPath = path.resolve(__dirname, 'to-upstream-wpts');
 
 const filterGlobs = process.argv.length >= 3 ? process.argv.slice(2) : ['**/*.html'];
 const workerTestPattern = /\.(?:dedicated|shared|service)worker(?:\.https)?\.html$/;
@@ -34,30 +33,24 @@ process.on('rejectionHandled', promise => {
   rejections.delete(promise);
 });
 
-let totalFailures = 0;
-wptRunner(toUpstreamTestsPath, { rootURL: 'streams/', setup, filter })
-  .then(failures => {
-    totalFailures += failures;
-    return wptRunner(testsPath, { rootURL: 'streams/', setup, filter });
-  })
-  .then(failures => {
-    totalFailures += failures;
-    process.exitCode = totalFailures;
+wptRunner(testsPath, { rootURL: 'streams/', setup, filter })
+    .then(failures => {
+      process.exitCode = failures;
 
-    if (rejections.size > 0) {
-      if (totalFailures === 0) {
-        process.exitCode = 1;
-      }
+      if (rejections.size > 0) {
+        if (failures === 0) {
+          process.exitCode = 1;
+        }
 
-      for (const reason of rejections.values()) {
-        console.error('Unhandled promise rejection: ', reason.stack);
+        for (const reason of rejections.values()) {
+          console.error('Unhandled promise rejection: ', reason.stack);
+        }
       }
-    }
-  })
-  .catch(e => {
-    console.error(e.stack);
-    process.exitCode = 1;
-  });
+    })
+    .catch(e => {
+      console.error(e.stack);
+      process.exitCode = 1;
+    });
 
 function setup(window) {
   // Necessary so that we can send test-realm promises to the jsdom-realm implementation without causing assimilation.
