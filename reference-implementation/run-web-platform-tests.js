dfedb87683a4e3b6c73c58adb0b9dff11797be5d
Mattias Buelens <mattias@buelens.com>
2023-06-07 16:43:38 +0200

    Re-enable garbage collection tests

--- reference-implementation/run-web-platform-tests.js
+++ reference-implementation/run-web-platform-tests.js
@@ -37,7 +37,6 @@ async function main() {
     // These tests use ArrayBuffers backed by WebAssembly.Memory objects, which *should* be non-transferable.
     // However, our TransferArrayBuffer implementation cannot detect these, and will incorrectly "transfer" them anyway.
     'readable-byte-streams/non-transferable-buffers.any.html',
-    'readable-streams/garbage-collection.any.html', // FIXME: reenable this test once server provides common/gc.js.
     'readable-streams/owning-type-message-port.any.html', // disabled due to MessagePort use.
     'readable-streams/owning-type-video-frame.any.html', // disabled due to VideoFrame use.
     'readable-streams/owning-type.any.html', // FIXME: reenable this test once owning type PR lands.
